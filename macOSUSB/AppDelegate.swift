a0f8e3a610ccd02fff201c4284f7577143902396
Filippo Claudi <filippo.claudi@gmail.com>
2019-01-06 13:06:34 +0100

    Initial Commit

---
+++ macOSUSB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOSUSB
+//
+//  Created by Filippo Claudi on 06/01/2019.
+//  Copyright © 2019 Filippo Claudi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOSUSB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSUSB
+//
+//  Created by Filippo Claudi on 06/01/2019.
+//  Copyright © 2019 Filippo Claudi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
